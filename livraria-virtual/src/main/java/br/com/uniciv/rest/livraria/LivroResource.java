2e282ce0d6ed4b88b30cc7eac0954290f5bec4da
Eduardo Rodrigues Santana <mitinickk@gmail.com>
2026-06-23 20:46:09 -0300

    Adicionado try/catch para capturar exception no metodo que reotorna livro por seu isbn

--- livraria-virtual/src/main/java/br/com/uniciv/rest/livraria/LivroResource.java
+++ livraria-virtual/src/main/java/br/com/uniciv/rest/livraria/LivroResource.java
@@ -4,24 +4,31 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.Status;
 
 @Path("livro")
 public class LivroResource {
 
 	private LivroRepositorio livroRepositorio = new LivroRepositorio();
-	
+
 	@GET
-	@Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-	public Livros getLivros(){
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public Livros getLivros() {
 		Livros livros = new Livros();
 		livros.setLivros(livroRepositorio.getLivros());
 		return livros;
 	}
-	
+
 	@GET
 	@Path("/{isbn}")
 	public Livro getLivroPorIsbn(@PathParam("isbn") String isbn) {
-		return livroRepositorio.getLivroPorIsbn(isbn);
+		try {
+			return livroRepositorio.getLivroPorIsbn(isbn);
+
+		} catch(LivroNaoEncontradoException e) {
+			throw new WebApplicationException(Status.NOT_FOUND);
+		}
 	}
 }

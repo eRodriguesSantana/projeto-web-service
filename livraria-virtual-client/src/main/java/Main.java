b2c04d6ab2b5e6f1f183b370aac2155fbdb7f084
Eduardo Rodrigues Santana <mitinickk@gmail.com>
2026-06-26 02:50:27 -0300

    Ajuste no client para criptografia, basic auth e requisicao https com sucesso

--- livraria-virtual-client/src/main/java/Main.java
+++ livraria-virtual-client/src/main/java/Main.java
@@ -1,11 +1,30 @@
+import javax.net.ssl.SSLContext;
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.SslConfigurator;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Livros livros = ClientBuilder.newClient()
-		.target("http://localhost:8080/livraria-virtual")
+		SslConfigurator config = SslConfigurator
+				.newInstance()
+				.trustStoreFile("server.keystore")
+				.trustStorePassword("livraria");
+		
+		SSLContext context = config.createSSLContext();
+		
+		Client client = ClientBuilder.newBuilder().sslContext(context).build();
+		
+		HttpAuthenticationFeature auth = 
+				HttpAuthenticationFeature.basic("admin", "password");
+		
+		client.register(auth);
+		
+		Livros livros = client
+		.target("https://localhost:8443/livraria-virtual")
 		.path("livro")
 		.request(MediaType.APPLICATION_XML)
 		.get(Livros.class);

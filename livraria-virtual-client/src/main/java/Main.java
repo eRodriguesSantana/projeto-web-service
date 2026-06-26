615c385f975aefee30ebbbb073d498d6656b123b
Eduardo Rodrigues Santana <mitinickk@gmail.com>
2026-06-25 22:21:51 -0300

    Criacao de projeto client com Jax-RS pra consumir as APIs do projeto livraria-virtual

---
+++ livraria-virtual-client/src/main/java/Main.java
@@ -0,0 +1,18 @@
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Livros livros = ClientBuilder.newClient()
+		.target("http://localhost:8080/livraria-virtual")
+		.path("livro")
+		.request(MediaType.APPLICATION_XML)
+		.get(Livros.class);
+		
+		for(Livro livro : livros.getLivros()) {
+			System.out.println(livro.getTitulo());
+		}
+	}
+
+}
